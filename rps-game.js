a3c53463af601f69ae5b07d8bf8dd9b1e125c8d1
Marko <markcuk000@gmail.com>
2024-06-26 16:47:08 +0200

    .

--- rps-game.js
+++ rps-game.js
@@ -1,1 +1,18 @@
-console.log(1 + 1);
+let getHumanChoice = prompt ("Enter:", "Rock, Paper or Scissors");
+
+const getComputerChoice=["Rock", "Paper" , "Scissors"];
+const random = Math.floor(Math.random() * getComputerChoice.length);
+console.log(random, getComputerChoice[random]);
+
+alert ("Computer chose" + " "+ getComputerChoice[random]);
+
+
+var humanScore = 0;
+var computerScore = 0;
+
+const playRound = (humanChoice, computerChoice)
+    
+humanScore = humanScore.toLowerCase();
+
+
+
